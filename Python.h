dd4b04b30d362fa2c4a589c7908b1f6d6b57f9fd
SilverCoin00 <dang.nguyen.sp@gmail.com>
2025-07-08 17:12:46 +0700

    Update Python.h

--- Python.h
+++ Python.h
@@ -6,12 +6,12 @@
 #include <string.h>
 #include <math.h>
 #include <time.h>
-#include "D:\Std_gcc_&_g++_plus\Python_h\Dsa.h"
-#include "D:\Std_gcc_&_g++_plus\Python_h\Numpy.h"
-#include "D:\Std_gcc_&_g++_plus\Python_h\Pandas.h"
-#include "D:\Std_gcc_&_g++_plus\Python_h\Sklearn.h"
-#include "D:\Std_gcc_&_g++_plus\Python_h\Dataset_2.h"
-#include "D:\Std_gcc_&_g++_plus\Python_h\Dataset_3.h"
-#include "D:\Std_gcc_&_g++_plus\Python_h\Dataset.h"
+#include "\Python_h\Dsa.h"
+#include "\Python_h\Numpy.h"
+#include "\Python_h\Pandas.h"
+#include "\Python_h\Sklearn.h"
+#include "\Python_h\Dataset_2.h"
+#include "\Python_h\Dataset_3.h"
+#include "\Python_h\Dataset.h"
 
 #endif
